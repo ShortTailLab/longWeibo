b8c90b28660609069b7f5fb21d959a674bde38f7
Zeyang Li <a.banknote@gmail.com>
2012-06-10 15:29:06 +0800

    merged render code

--- client/server.py
+++ client/server.py
@@ -1,7 +1,10 @@
+import datetime, time
+import json
+import subprocess
 import tornado.httpserver, tornado.ioloop, tornado.options, tornado.web, os.path 
-from tornado.options import define, options
+from tornado.options import define, options, parse_command_line
 
-define("port", default=8888, help="run on the given port", type=int)
+define("port", default=8000, help="run on the given port", type=int)
 
 def file_path(relative) :
     return os.path.join(os.path.dirname(__file__), relative)
@@ -35,16 +38,49 @@ class UploadImage(tornado.web.RequestHandler):
 
         self.finish(resp)
 
+
+global fid
+fid=0
+
 # Json
 class Render(tornado.web.RequestHandler):
-    def post(self):
+    def post(self,*args,**kwargs):
         content = self.get_argument('content')
         html = tornado.template.Loader(file_path("")).load("_render.html").generate( content = content )
         print html
+
+        #format file name
+        global fid
+        if (fid>99999):
+            fid=0
+        else:
+            fid=fid+1
+        now=datetime.datetime.now()
+        filename = now.strftime("%Y%m%dT%H%M%S") + "F" + str(fid)
+        src_file = file_path('static/render/' + filename + '.html')
+        output_file = file_path('static/render/' + filename + '.jpg')
+
+        print "====================="
+        print "assgning filename and saveing file"
+
+        f = open(src_file,'w')
+        f.write(html)
+        f.close()
+
+        print src_file + 'written'
+
+        print "====================="
+        print "running HTML->IMG"
+        print "filename: " + filename
+        subprocess.call(["./../bin/wkhtmltoimage-amd64", src_file, output_file])
+        print "====================="
+        print "returning..."
+        
         resp = {    'success' : True,
                     'error' : None,
-                    'image_url' : '/static/render/'
+                    'image_url' : '/static/render/' + filename + '.jpg'
                 }
+       
         self.finish( resp )
 
 settings ={
@@ -60,5 +96,6 @@ application = tornado.web.Application([
 ], **settings);
 
 if __name__ == "__main__":
+    parse_command_line()
     application.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
